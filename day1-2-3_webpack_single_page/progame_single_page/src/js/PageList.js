46c51858159a4625200b4d44477006ec4b7abd42
totycos <dv94@hotmail.fr>
2024-01-29 17:43:03 +0100

    Commit WEBPACK_SPA day 1 - Add platform logos

--- day1-2-3_webpack_single_page/progame_single_page/src/js/PageList.js
+++ day1-2-3_webpack_single_page/progame_single_page/src/js/PageList.js
@@ -12,10 +12,13 @@ const PageList = (argument = '') => {
             </div>
             <div class="imgBlock__info">
               <p>${article.released}</p>
-              <p>Editeur : </p>
+              <p>Editeur : ?????</p>
+              <p>${article.rating}/5 - ${article.ratings_count} votes</p>
+              <p class="info__genre">${article.genres.map(genre => genre.name).join(', ')}
             </div>
           </div>
           <a href="#pagedetail/${article.id}"><h3>${article.name}</h3></a>
+          <div class="cardGame__platform">${buildPlatforms(article.parent_platforms.map(e => e.platform.slug))}</div>
         </article>`
       ));
       const resultsContainer = document.querySelector('.page-list .articles');
@@ -35,21 +38,6 @@ const PageList = (argument = '') => {
     // https://api.rawg.io/api/games?key=7f052a404021482ca44e2012ad28545d
   };
 
-  const addHoverEffect = () => {
-    const imgBlocks = document.querySelectorAll('.imgBlock__img');
-
-    imgBlocks.forEach((imgBlock) => {
-      imgBlock.addEventListener('mouseenter', () => {
-        console.log('HELLO')
-        imgBlock.classList.add('hovered');
-      });
-
-      imgBlock.addEventListener('mouseleave', () => {
-        imgBlock.classList.remove('hovered');
-      });
-    });
-  };
-
   const render = () => {
     pageContent.innerHTML = `
       <section class="page-list">
@@ -58,10 +46,35 @@ const PageList = (argument = '') => {
     `;
 
     preparePage();
-    addHoverEffect();
   };
 
   render();
 };
 
   export { PageList };
+
+  const buildPlatforms = (platformsArray) => {
+    let svgs = []
+    platformsArray.forEach((element) => {
+      if (element === "playstation")
+       svgs.push(`<img src='src/assets/images/ps4.svg' alt='ps4 logo'>`)
+
+       if (element === "xbox")
+       svgs.push(`<img src='src/assets/images/xbox.svg' alt='xbox logo'>` )
+
+       if (element === "pc")
+       svgs.push(`<img src='src/assets/images/windows.svg' alt='windows logo'>`)
+
+       if (element === "nintendo")
+       svgs.push(`<img src='src/assets/images/switch.svg' alt='switch logo'>`)
+
+       if (element === "mac")
+       svgs.push(`<img src='src/assets/images/linux.svg' alt='linux logo'>`)
+
+       if (element === "android" || element === "ios")
+       svgs.push(`<img src='src/assets/images/mobile.svg' alt='mobile logo'>`)
+
+       return ""
+    })
+    return svgs.join("") 
+  }
